17d5663f6ed2b3424a29cb4a8d96a30b3effd062
videoyt24 <161769482+videoyt24@users.noreply.github.com>
2024-03-11 17:01:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,17 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['234704535241', 'EXCEL', true], 
-  ['2348021779696', 'EXCEL', true], 
-  ['234704503541', '⚡Excel', true],
+  ['255688930205', 'CEO', true], 
+  ['255716312895', '⚡xHRISS', true],
 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2341', '2348021779696', '2347045035241','233533763772'] 
-global.prems = ['2347045035241','233208179431','233533763772']
-global.allowed = ['2348021779696', '2347045035241']
+global.mods = ['255', '255688930205','255716312895'] 
+global.prems = ['255688930205','255716312895']
+global.allowed = ['255688930205', '255716312895']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -52,9 +51,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'STAR-MD-V2'
+global.botname = 'BOT-MD'
 global.premium = 'true'
-global.packname = 'STAR-MD' 
+global.packname = 'BOT-MD' 
 global.author = '@Excel' 
 global.menuvid = 'https://i.imgur.com/BPqmxsm.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
@@ -66,7 +65,7 @@ global.fglog = 'STAR.jpg'
 global.thumb = fs.readFileSync('./STAR.jpg')
 
 
-global.wait = '*🕣 _STAR IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.wait = '*🕣 _BOT IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
 global.rwait = '🔜'
 global.dmoji = '⚡'
 global.done = '☑️'
